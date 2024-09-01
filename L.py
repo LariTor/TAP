3746e15f1e70613f7c7bf3836fdb30baa37cf941
Mirko <mirkopoj@hotmail.com>
2024-09-01 00:01:04 -0300

    Logica Ludica bien, pero con runtime error

--- L.py
+++ L.py
@@ -1,24 +1,6 @@
 import sys
 
 
-def is_pow(x):
-    print(x.bit_length())
-    print(x.bit_count())
-    print(1 << 5)
-    return 1 << (x.bit_count()-1) == x
-
-
-v = 3
-print(is_pow(v))
-v = 2
-print(is_pow(v))
-v = 32
-print(is_pow(v))
-v = 31
-print(is_pow(v))
-v = 33
-print(is_pow(v))
-
 n, q = [int(x) for x in sys.stdin.readline().split()]
 A = [int(x) for x in sys.stdin.readline().split()]
 
@@ -28,18 +10,42 @@ for _ in range(q):
     juegos.append((int(inp[0]), int(inp[1])))
 
 
-def jugar(juego):
-    a = 0
-    b = 0
-    u = 0
-    p = 0
-    i = 0
-    for v in juego:
-        if v == 1:
-            u += 1
-        if v % 2 == 1:
-            i += 1
-        if v > 1 and v >> (v.bit_count()-1) == 1:
-            p += 1
-            a += 1
-            b += 1
+def is_pow(x):
+    return x.bit_count() == 1
+
+
+unos = [0]
+potencias = [0]
+impares = [0]
+
+u = 0
+p = 0
+i = 0
+for v in A:
+    if v == 1:
+        u += v
+    elif v % 2 == 1:
+        i += v
+    elif is_pow(v):
+        p += v
+    unos.append(u)
+    impares.append(i)
+    potencias.append(p)
+
+
+def jugar(L, R):
+    u = unos[R] - unos[L]
+    p = potencias[R] - potencias[L]
+    i = impares[R] - impares[L]
+    a = ((u//2) + (u % 2)) + p
+    b = (u//2) + i
+    if a > b:
+        return "A"
+    if a == b:
+        return "E"
+    if a < b:
+        return "B"
+
+
+for L, R in juegos:
+    print(jugar(L-1, R))
